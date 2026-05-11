2921e01df30f06e17edd53eafef91251401b36ab
botwijns <siebgerben@gmail.com>
2026-05-11 11:07:42 +0200

    add typo fix

--- vite.prototype_1.config.ts
+++ vite.prototype_1.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+    root: 'prototype_1',
     base: '/endprojectSandbox/prototype_1/',
     build: {
         outDir: 'dist/prototype_1',
--- vite.prototype_2.config.ts
+++ vite.prototype_2.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+    root: 'prototype_2',
     base: '/endprojectSandbox/prototype_2/',
     build: {
         outDir: 'dist/prototype_2',
