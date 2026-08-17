b4373faab930066027fb381d9aa97e855e35556e
botwijns <siebgerben@gmail.com>
2026-08-17 15:44:22 +0200

    adapt small bug

--- vite.prototype_6.config.ts
+++ vite.prototype_6.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-    root: 'prototype_5',
+    root: 'prototype_6',
     base: '/endprojectSandbox/prototype_6/',
     build: {
         outDir: 'dist/prototype_6',
