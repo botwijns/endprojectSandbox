3dc48c3780c3bac85f81a093304bce9d85a14004
botwijns <siebgerben@gmail.com>
2026-08-17 14:29:23 +0200

    add missing prototype config file

---
+++ vite.prototype_6.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+    root: 'prototype_5',
+    base: '/endprojectSandbox/prototype_6/',
+    build: {
+        outDir: 'dist/prototype_6',
+        emptyOutDir: false
+    }
+})
